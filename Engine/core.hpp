168e0c8ac511701922ac4b325d4791468db9d032
Arman Imani <armanimani87@gmail.com>
2019-09-28 22:25:11 +0200

    Put some information about the macro. introduced the typedefs for new types that are being used

--- Engine/core.hpp
+++ Engine/core.hpp
@@ -1,7 +1,20 @@
 #pragma once
 
+#include <cstddef>
+
 #ifdef NOVA_BUILD_DLL
+/**
+	 * \brief Defines the macro that is used to import/export DLL
+	 */
 	#define NOVA_API __declspec(dllexport)
 #else
 	#define NOVA_API __declspec(dllimport)
 #endif
+
+
+using Int8 = __int8;
+using Int16 = __int16;
+using Int32 = __int32;
+using Int64 = __int64;
+
+using Float = float;

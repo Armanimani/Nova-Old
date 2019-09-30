5f515dae3a2d08ff8b2d68de71ae89cfb47e7913
Arman Imani <armanimani87@gmail.com>
2019-09-30 23:16:14 +0200

    Added editor project

---
+++ Editor/Forms/NovaEditorForm.Designer.cs
@@ -0,0 +1,34 @@
+﻿namespace Editor
+{
+    partial class frmNovaEditor
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // 
+            // frmNovaEditor
+            // 
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
+

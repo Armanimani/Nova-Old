64a616fce711a2fa2e4407b65bdb3351d90ca930
Arman Imani <armanimani87@gmail.com>
2019-10-04 01:01:42 +0200

    Added the main form and its title bar including the close, minimize, maximize and restore button

--- Editor/Forms/NovaEditorForm.Designer.cs
+++ Editor/Forms/NovaEditorForm.Designer.cs
@@ -21,14 +21,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmNovaEditor));
+            this.panelTitle = new System.Windows.Forms.Panel();
+            this.btnMinimize = new System.Windows.Forms.Button();
+            this.btnMaximizeWindow = new System.Windows.Forms.Button();
+            this.btnCloseWindow = new System.Windows.Forms.Button();
+            this.panelTitle.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTitle
+            // 
+            this.panelTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.panelTitle.Controls.Add(this.btnMinimize);
+            this.panelTitle.Controls.Add(this.btnMaximizeWindow);
+            this.panelTitle.Controls.Add(this.btnCloseWindow);
+            this.panelTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitle.Location = new System.Drawing.Point(0, 0);
+            this.panelTitle.Name = "panelTitle";
+            this.panelTitle.Size = new System.Drawing.Size(2296, 32);
+            this.panelTitle.TabIndex = 0;
+            // 
+            // btnMinimize
+            // 
+            this.btnMinimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.btnMinimize.BackgroundImage = global::Editor.Properties.Resources.window_minimize_32;
+            this.btnMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnMinimize.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnMinimize.FlatAppearance.BorderSize = 0;
+            this.btnMinimize.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.btnMinimize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.btnMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimize.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.btnMinimize.Location = new System.Drawing.Point(2176, 0);
+            this.btnMinimize.Margin = new System.Windows.Forms.Padding(0);
+            this.btnMinimize.Name = "btnMinimize";
+            this.btnMinimize.Size = new System.Drawing.Size(40, 32);
+            this.btnMinimize.TabIndex = 3;
+            this.btnMinimize.UseVisualStyleBackColor = false;
+            this.btnMinimize.Click += new System.EventHandler(this.btnMinimize_Click);
+            // 
+            // btnMaximizeWindow
+            // 
+            this.btnMaximizeWindow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.btnMaximizeWindow.BackgroundImage = global::Editor.Properties.Resources.window_maximize_32;
+            this.btnMaximizeWindow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnMaximizeWindow.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnMaximizeWindow.FlatAppearance.BorderSize = 0;
+            this.btnMaximizeWindow.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.btnMaximizeWindow.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.btnMaximizeWindow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMaximizeWindow.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.btnMaximizeWindow.Location = new System.Drawing.Point(2216, 0);
+            this.btnMaximizeWindow.Margin = new System.Windows.Forms.Padding(0);
+            this.btnMaximizeWindow.Name = "btnMaximizeWindow";
+            this.btnMaximizeWindow.Size = new System.Drawing.Size(40, 32);
+            this.btnMaximizeWindow.TabIndex = 2;
+            this.btnMaximizeWindow.UseVisualStyleBackColor = false;
+            this.btnMaximizeWindow.Click += new System.EventHandler(this.btnMaximizeWindow_Click);
+            // 
+            // btnCloseWindow
+            // 
+            this.btnCloseWindow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.btnCloseWindow.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnCloseWindow.BackgroundImage")));
+            this.btnCloseWindow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnCloseWindow.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btnCloseWindow.FlatAppearance.BorderSize = 0;
+            this.btnCloseWindow.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DodgerBlue;
+            this.btnCloseWindow.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.btnCloseWindow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCloseWindow.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.btnCloseWindow.Location = new System.Drawing.Point(2256, 0);
+            this.btnCloseWindow.Margin = new System.Windows.Forms.Padding(0);
+            this.btnCloseWindow.Name = "btnCloseWindow";
+            this.btnCloseWindow.Size = new System.Drawing.Size(40, 32);
+            this.btnCloseWindow.TabIndex = 1;
+            this.btnCloseWindow.UseVisualStyleBackColor = false;
+            this.btnCloseWindow.Click += new System.EventHandler(this.btnCloseWindow_Click);
             // 
             // frmNovaEditor
             // 
-            this.PerformLayout();
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.ClientSize = new System.Drawing.Size(2296, 1483);
+            this.Controls.Add(this.panelTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmNovaEditor";
+            this.panelTitle.ResumeLayout(false);
+            this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelTitle;
+        private System.Windows.Forms.Button btnMaximizeWindow;
+        private System.Windows.Forms.Button btnCloseWindow;
+        private System.Windows.Forms.Button btnMinimize;
     }
 }
 
--- Editor/Forms/NovaEditorForm.cs
+++ Editor/Forms/NovaEditorForm.cs
@@ -7,15 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Controls;
 
 namespace Editor
 {
-    public partial class frmNovaEditor : NovaForm
+    public partial class frmNovaEditor : Form
     {
         public frmNovaEditor()
         {
             InitializeComponent();
+        }
+
+        private void btnCloseWindow_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnMaximizeWindow_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Maximized)
+            {
+                WindowState = FormWindowState.Normal;
+                btnMaximizeWindow.BackgroundImage = Properties.Resources.window_restore_32;
+            }
+            else
+            {
+                WindowState = FormWindowState.Maximized;
+                btnMaximizeWindow.BackgroundImage = Properties.Resources.window_maximize_32;
+            }
+        }
+
+        private void btnMinimize_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
         }
     }
 }
